22fe1c9b1f36880ca45210af8e3f387217746933
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2013-03-11 10:48:03 -0300

    Carga inicial del proyecto

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_spd_join_project do
+  name 'Redmine SPD Join Project plugin'
+  author 'Maximiliano Dello Russo'
+  description 'Este plugin permite que un usuario no miembro de un proyecto pueda autoincluirse en el mismo'
+  version '0.0.1'
+  url 'https://github.com/maxidr/redmine_spd_join_project'
+  author_url 'https://coderwall.com/maxidr'
+end
