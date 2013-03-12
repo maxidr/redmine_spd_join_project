5c46be295ebaf588c20c4af4021de4cb073c6e41
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2013-03-12 14:48:51 -0300

    Se corrige un error al generar le link (no incluía la sub-uri)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-post 'projects/:project_id/join', :to => 'join_to_project#create', :as => 'join_to_project'
+post '/projects/:project_id/join', :to => 'join_to_project#create', :as => 'join_to_project'
